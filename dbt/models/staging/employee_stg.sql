e81090c7683957afc68983d74ef36153d135ebe0
John Bassey <jsbazman@gmail.com>
2022-06-29 11:41:27 +0100

    created products.sql file

--- dbt/models/staging/employee_stg.sql
+++ dbt/models/staging/employee_stg.sql
@@ -2,6 +2,6 @@ SELECT
     employee_id,
     CONCAT(first_name, ' ', last_name) as full_name,
     department
-FROM {{source('whiskey', 'customers')}}
-JOIN {{source('whiskey', 'departments')}}
+FROM {{source ('whiskey', 'customers') }}
+JOIN {{source ('whiskey', 'departments') }}
 USING(department_id)
