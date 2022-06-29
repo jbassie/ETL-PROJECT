c84c28b2b1e9f234d0ba98805ce6b31a75bc386c
John Bassey <jsbazman@gmail.com>
2022-06-29 12:43:27 +0100

    delete dagster

--- dbt/models/core/final_1.sql
+++ dbt/models/core/final_1.sql
@@ -24,6 +24,7 @@ final as ( SELECT *
         JOIN payments
         USING(customer_id)
         JOIN products
-        USING(product_id) )
+        USING(product_id)
+        where date > 2010 )
 
 select * from final
